5f414bcb41f884ddf8981834d095e7d44265b919
awesomedude123-ai <66534860+awesomedude123-ai@users.noreply.github.com>
2025-05-08 10:53:18 -0400

    ReAdded IST411 Project

---
+++ IST411Project/frontend/src/redux/clubRelated/clubHandle.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import {
+    getFailedTwo,
+    getSuccess,
+    getError,
+    getRequest,
+    authRequest,
+    authFailed,
+    stuffAdded,
+    authError,
+    getTypesSuccess,
+    getFailed,
+    detailsSuccess,
+    getDeleteSuccess
+}from './clubSlice.js';
+
+
+export const getAllClubs = (id, address) => async (dispatch) => {
+    dispatch(getRequest());
+
+    try {
+        const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/${address}List/${id}`);
+        if (result.data.message) {
+            dispatch(getFailedTwo(result.data.message));
+        } else {
+            dispatch(getSuccess(result.data));
+        }
+    } catch (error) {
+        dispatch(getError(error));
+    }
+};
+
+export const addStuff = (fields, address) => async (dispatch) => {
+    dispatch(authRequest());
+    try {
+        const result = await axios.post(`${process.env.REACT_APP_BASE_URL}/${address}Create`, fields, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (result.data.message) {
+            dispatch(authFailed(result.data.message));
+        } else {
+            dispatch(stuffAdded(result.data));
+        }
+    } catch (error) {
+        dispatch(authError(error));
+    }
+};
+
+export const getClubDetails = (id, address) => async (dispatch) => {
+    dispatch(getRequest());
+
+    try {
+        const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/${address}/${id}`);
+        if (result.data) {
+            dispatch(detailsSuccess(result.data));
+        }
+    } catch (error) {
+        dispatch(getError(error));
+    }
+};
